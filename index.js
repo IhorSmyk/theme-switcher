2d36f019b1d5c1868bd1d729fd05327ce14bfc6f
Ihor Smyk <ismyk256@gmail.com>
2022-12-08 15:17:28 +0200

    q

--- index.js
+++
@@ -1,9 +0,0 @@
-
-
-let inputRef = document.querySelector("#dark-mode");
-
-
-// console.log(inputRef.getComputedStyle());
-
-
-console.log(window.getComputedStyle(inputRef, ":before").colorInterpolation);
