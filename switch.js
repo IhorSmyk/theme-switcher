7766351e1045dcfe4c159125fca6727fa3da79c8
Ihor Smyk <ismyk256@gmail.com>
2022-12-08 18:02:32 +0200

    upd

--- switch.js
+++ switch.js
@@ -1,16 +1,14 @@
-const refs = {
+const formToggleRefs = {
+    labels: document.querySelectorAll(".label-toggle"),
     toggle: document.querySelector(".toggle"),
-    body: document.querySelector("body"),
-}
+};
 
-refs.toggle.addEventListener("change", () => {refs.body.style.backgroundColor = "#000000"; });
-
-
-
-
-// if (refs.toggle.hasAttribute("checked")) {
-//     refs.body.style.backgroundColor = "#ff0000";
-// }
-
+formToggleRefs.toggle.addEventListener("change", () => {
+    document.body.classList.toggle("js-dark-theme");
+    formToggleRefs.labels[0].classList.toggle("label-light-toggle-light-theme");
+    formToggleRefs.labels[0].classList.toggle("label-light-toggle-dark-theme");
+    formToggleRefs.labels[1].classList.toggle("label-dark-toggle-light-theme");
+    formToggleRefs.labels[1].classList.toggle("label-dark-toggle-dark-theme");
+});
 
 
